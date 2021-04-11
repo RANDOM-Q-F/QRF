7822a41f10ac5866b3aa79604cc765a96226910a
noureddein <noureddein@gmail.com>
2021-04-11 21:31:58 +0300

    Filter by Dropdown list Donr

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 'use struct';
+
+$(document).ready(function () {
+  $('#list').change(function () {
+    if ($(this).val() === 'All Quotes') {
+      $('.eachContainerQuote').show();
+    } else {
+      $('.eachContainerQuote').hide();
+      $('.' + $(this).val()).show();
+    }
+  });
+});
+
--- server.js
+++ server.js
@@ -15,6 +15,8 @@ const app = express();
 app.use(cors());
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.static('js'));
+
 const client = new pg.Client(process.env.DATABASE_URL);
 const PORT = process.env.PORT || 3001;
 // app.use(methodOverride('_method'));
@@ -31,72 +33,72 @@ app.post('/addlike', handleLike);
 
 //Port listener ===================================================
 app.listen(PORT, () => {
-    console.log(`Listening at ${PORT}`);
+  console.log(`Listening at ${PORT}`);
 });
 
 
 //Routes Functions =================================================
 // Home Function
 function handleHome(req, res) {
-    res.render('index');
+  res.render('index');
 }
 
 
 function handleAbout(req, res) {
-    res.render('about-us');
+  res.render('about-us');
 }
 
 // Quotes Function
 function handleQuotes(req, res) {
-    const url = 'https://goquotes-api.herokuapp.com/api/v1/random?count=15';
+  const url = 'https://goquotes-api.herokuapp.com/api/v1/random?count=25';
 
-    superagent.get(url)
-        .then(apiResponse => apiResponse.body.quotes.map(quote => new Quote(quote)))
-        .then(resultObjects => res.render('quote', { allQuotes: resultObjects }))
-        .catch(error => {
-            console.log("Promise Rejection Error Being Handled");
-            // response.render('pages/error', { errorText: error });
-        });
+  superagent.get(url)
+    .then(apiResponse => apiResponse.body.quotes.map(quote => new Quote(quote)))
+    .then(resultObjects => res.render('quote', { allQuotes: resultObjects }))
+    .catch(error => {
+      console.log('Promise Rejection Error Being Handled');
+      // response.render('pages/error', { errorText: error });
+    });
 }
 
 // Constructor Functions ============================================
 function Quote(data) {
-    this.quote = data.text;
-    this.author = data.author;
-    this.category = data.tag;
-    this.liked = false;
+  this.quote = data.text;
+  this.author = data.author;
+  this.category = data.tag;
+  this.liked = false;
 }
 
 // Favorite Function ================================================
 function handleFav(req, res) {
-    res.render('favorite');
+  res.render('favorite');
 }
 
 // Like Function ================================================
 function handleLike(req, res) {
 
-    const quote = req;
-    console.log(quote);
+  const quote = req;
+  console.log(quote);
 
-    // const SQL = 'INSERT INTO quotes (quote, author, category, liked) VALUES ($1, $2, $3, $4);';
+  // const SQL = 'INSERT INTO quotes (quote, author, category, liked) VALUES ($1, $2, $3, $4);';
 
-    // let values = [quote.quote, quote.author, quote.category, true];
+  // let values = [quote.quote, quote.author, quote.category, true];
 
-    // // const SQL2 = 'UPDATE quotes SET liked = $1 WHERE id = $2;';
+  // // const SQL2 = 'UPDATE quotes SET liked = $1 WHERE id = $2;';
 
-    // // let values2 = [true, "parameter"];
+  // // let values2 = [true, "parameter"];
 
-    // client.query(SQL, values)
-    //     .then(result => response.redirect(`/`))
-    //     .catch(error => {
-    //         console.log("Promise Rejection Error Being Handled");
-    //         // response.render('pages/error', { errorText: error });
-    //     });
+  // client.query(SQL, values)
+  //     .then(result => response.redirect(`/`))
+  //     .catch(error => {
+  //         console.log("Promise Rejection Error Being Handled");
+  //         // response.render('pages/error', { errorText: error });
+  //     });
 
 
 }
 
 //Error function =====================================================
 function error(err) {
-    return `Oops! Something Went Wrong ${err}`;
+  return `Oops! Something Went Wrong ${err}`;
 }
