f2492e7e1c27d2c24744c19ab9450b8081eb98bc
Reema Eilouti <reema.eilouti@gmail.com>
2021-04-11 19:31:25 +0300

    added database and started the like function

--- schema.sql
+++ schema.sql
@@ -3,5 +3,9 @@ CREATE TABLE IF NOT EXISTS quotes(
   id SERIAL PRIMARY KEY,
   quote TEXT NOT NULL,
   author VARCHAR (255),
-  category VARCHAR (255)
+  category VARCHAR (255),
+  liked BOOLEAN
 );
+
+INSERT INTO quotes (quote, author, category, liked) 
+VALUES('Hello, World!', 'Team301', 'Proof of Life', false);
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const express = require('express');
 const superagent = require('superagent');
 const pg = require('pg');
 const cors = require('cors');
+const methodOverride = require('method-override');
+
 
 
 //Middleware =====================================================
@@ -15,64 +17,86 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 const client = new pg.Client(process.env.DATABASE_URL);
 const PORT = process.env.PORT || 3001;
+// app.use(methodOverride('_method'));
+
 
 
 //Handel Routes ==================================================
 app.get('/', handleHome);
-// app.get('/favorites', handleFav);
+app.get('/favorites', handleFav);
 app.get('/aboutus', handleAbout);
-app.get('/quote', handleFQ);
+app.get('/quote', handleQuotes);
+app.post('/addlike', handleLike);
 
 
 //Port listener ===================================================
 app.listen(PORT, () => {
-  console.log(`i'm listening at ${PORT}`);
+    console.log(`Listening at ${PORT}`);
 });
 
 
 //Routes Functions =================================================
 // Home Function
 function handleHome(req, res) {
-  res.render('index');
+    res.render('index');
 }
 
-// Favorite Function
-// function handleFav(req, res){
-//   res.send('i am running');
-// }
-function handleAbout(req, res){
-  res.render('about-us');
+
+function handleAbout(req, res) {
+    res.render('about-us');
 }
 
 // Quotes Function
-function handleFQ(req, res) {
-  const arr = [];
-  const url = 'https://goquotes-api.herokuapp.com/api/v1/random?count=15';
-  superagent.get(url)
-    .then(results => {
-      const data = results.body.quotes;
-      //Adding quotes to the array to make the drop list
-      data.forEach(element => {
-        if (!arr.includes(element.tag) && element.tag) {
-          arr.push(element.tag);
-        }
-      });
+function handleQuotes(req, res) {
+    const url = 'https://goquotes-api.herokuapp.com/api/v1/random?count=15';
 
-      res.render('quote', {
-        list: arr,
-        allQuote: data
-      });
-    }).catch((err) => { error(err); });
+    superagent.get(url)
+        .then(apiResponse => apiResponse.body.quotes.map(quote => new Quote(quote)))
+        .then(resultObjects => res.render('quote', { allQuotes: resultObjects }))
+        .catch(error => {
+            console.log("Promise Rejection Error Being Handled");
+            // response.render('pages/error', { errorText: error });
+        });
 }
 
 // Constructor Functions ============================================
 function Quote(data) {
-  this.quote = data.text;
-  this.author = data.author;
-  this.category = data.tag;
+    this.quote = data.text;
+    this.author = data.author;
+    this.category = data.tag;
+    this.liked = false;
+}
+
+// Favorite Function ================================================
+function handleFav(req, res) {
+    res.render('favorite');
+}
+
+// Like Function ================================================
+function handleLike(req, res) {
+
+    const quote = req;
+    console.log(quote);
+
+    // const SQL = 'INSERT INTO quotes (quote, author, category, liked) VALUES ($1, $2, $3, $4);';
+
+    // let values = [quote.quote, quote.author, quote.category, true];
+
+    // // const SQL2 = 'UPDATE quotes SET liked = $1 WHERE id = $2;';
+
+    // // let values2 = [true, "parameter"];
+
+    // client.query(SQL, values)
+    //     .then(result => response.redirect(`/`))
+    //     .catch(error => {
+    //         console.log("Promise Rejection Error Being Handled");
+    //         // response.render('pages/error', { errorText: error });
+    //     });
+
+
 }
 
 //Error function =====================================================
 function error(err) {
-  return `Oops! Something Went Wrong ${err}`;
+    return `Oops! Something Went Wrong ${err}`;
 }
