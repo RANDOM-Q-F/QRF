8af12b2dadea31c2f2c94ad5b1bbe9fc85969254
m7madmomani2 <mohammad.momani1996.kk@gmail.com>
2021-04-13 07:49:04 +0300

    fix the connection of the database , handel favorite function, handle like function

--- server.js
+++ server.js
@@ -14,10 +14,13 @@ const methodOverride = require('method-override');
 const app = express();
 app.use(cors());
 app.set('view engine', 'ejs');
+app.use(express.static('img'));
 app.use(express.static('public'));
 app.use(express.static('js'));
-
-const client = new pg.Client(process.env.DATABASE_URL);
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true }));
+const DATABASE_URL = process.env.DATABASE_URL;
+const client = new pg.Client(DATABASE_URL);
 const PORT = process.env.PORT || 3001;
 // app.use(methodOverride('_method'));
 
@@ -25,17 +28,21 @@ const PORT = process.env.PORT || 3001;
 
 //Handel Routes ==================================================
 app.get('/', handleHome);
-app.get('/favorites', handleFav);
-app.get('/aboutus', handleAbout);
-app.get('/quote', handleQuotes);
+app.get('/favorite', handleFav);
+app.get('/about-us', handleAbout);
+app.get('/search', handleSearch);
 app.post('/addlike', handleLike);
-
+app.get('/quote', handleQuotes);
+app.use('*', (request, response) => response.status(404).send('This route does not exist'));
 
 //Port listener ===================================================
-app.listen(PORT, () => {
-  console.log(`Listening at ${PORT}`);
+client.connect().then(() => {
+  app.listen(PORT, () => {
+    console.log(`app is listning on port ${PORT}`);
+  });
+}).catch(err => {
+  console.log(`Sorry there is Database error ${err}`);
 });
-
 
 //Routes Functions =================================================
 // Home Function
@@ -48,17 +55,30 @@ function handleAbout(req, res) {
   res.render('about-us');
 }
 
-// Quotes Function
-function handleQuotes(req, res) {
-  const url = 'https://goquotes-api.herokuapp.com/api/v1/random?count=25';
+// search Function
+function handleSearch(req, res) {
+  // const countNum = req.query.search[1];
+  const value = req.query.search;
+  const type = req.query.searchType;
+  // const url = `https://goquotes-api.herokuapp.com/api/v1/random/${countNum}?type=${type}&val=${value}`;
+
+  const url = `https://goquotes-api.herokuapp.com/api/v1/all?type=${type}&val=${value}`;
+
 
   superagent.get(url)
     .then(apiResponse => apiResponse.body.quotes.map(quote => new Quote(quote)))
+    .then(resultObjects => res.render('searches', { allQuotes: resultObjects }))
+    .catch(error => { res.send(`Something Went Wrong ${error}`); });
+}
+
+
+// qoutes  function
+function handleQuotes(req, res) {
+  const url = `https://goquotes-api.herokuapp.com/api/v1/random?count=25`;
+  superagent.get(url)
+    .then(apiResponse => apiResponse.body.quotes.map(quote => new Quote(quote)))
     .then(resultObjects => res.render('quote', { allQuotes: resultObjects }))
-    .catch(error => {
-      console.log('Promise Rejection Error Being Handled');
-      // response.render('pages/error', { errorText: error });
-    });
+    .catch(error => { res.send(`Something Went Wrong ${error}`); });
 }
 
 // Constructor Functions ============================================
@@ -71,30 +91,32 @@ function Quote(data) {
 
 // Favorite Function ================================================
 function handleFav(req, res) {
-  res.render('favorite');
+  const sql = 'SELECT * FROM quotes;';
+  client.query(sql).then(dataDB => {
+  //   if (dataDB.rowCount) {
+
+    // res.render('favorite');
+
+      res.render('favorite', { quotes: dataDB.rows });
+  //   }else{
+  //     res.render('favorite', { quotes: dataDB.rows[0] });
+  //   }
+  }).catch(error=>{
+    res.send('notfound');
+  });
+
 }
 
 // Like Function ================================================
 function handleLike(req, res) {
-
-  const quote = req;
-  console.log(quote);
-
-  // const SQL = 'INSERT INTO quotes (quote, author, category, liked) VALUES ($1, $2, $3, $4);';
-
-  // let values = [quote.quote, quote.author, quote.category, true];
-
-  // // const SQL2 = 'UPDATE quotes SET liked = $1 WHERE id = $2;';
-
-  // // let values2 = [true, "parameter"];
-
-  // client.query(SQL, values)
-  //     .then(result => response.redirect(`/`))
-  //     .catch(error => {
-  //         console.log("Promise Rejection Error Being Handled");
-  //         // response.render('pages/error', { errorText: error });
-  //     });
-
+  const quote = req.body;
+  const SQL = 'INSERT INTO quotes (quote, author, category, liked) VALUES ($1, $2, $3, $4);';
+  let values = [quote.quote, quote.author, quote.category, true];
+  client.query(SQL, values)
+    .then(result => res.redirect(`/quote`))
+    .catch(error => {
+      console.log('Promise Rejection Error Being Handled');
+    });
 
 }
 
